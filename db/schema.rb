fc82e1d2b883c69a232a452a54ed9965d851abcd
Josue Costa <juncosta@gmail.com>
2025-05-31 12:56:27 -0300

    renames line_items to cart_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_30_200002) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_210858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "carts", force: :cascade do |t|
-    t.decimal "total_price", precision: 17, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "line_items", force: :cascade do |t|
+  create_table "cart_items", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.bigint "cart_id", null: false
     t.integer "quantity", default: 1, null: false
     t.decimal "current_price", precision: 17, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_line_items_on_cart_id"
-    t.index ["product_id"], name: "index_line_items_on_product_id"
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.decimal "total_price", precision: 17, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -38,6 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_30_200002) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "line_items", "carts"
-  add_foreign_key "line_items", "products"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
