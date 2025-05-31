e869f7c31727bfd42693044419820b4267080e35
Josue Costa <juncosta@gmail.com>
2025-05-31 13:23:14 -0300

    renames migration and add column abandoned

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_31_160439) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_31_161346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_31_160439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "last_interaction_at", null: false
+    t.boolean "abandoned", default: false, null: false
   end
 
   create_table "products", force: :cascade do |t|
